8c445a884c7299177e53ac57d604d2741350ba56
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2018-12-02 23:29:53 +0530

    Getting Source Code

--- main.c
+++ main.c
@@ -1,36 +1,67 @@
 #include <stdio.h>
-#include <libgen.h>
+#include <regex.h>
 #include <curl/curl.h>
 
+#define INTRANET_URL "http://intranet.daiict.ac.in/~daiict_nt01/"
 
-int main(int argc, char* args[]){
-
-  if(argc != 2){
-    printf("Please give url\n");
-    return 0;
-  }
-  // take url;
-  char *url = args[1];
-
+int main(void)
+{
   CURL *curl;
   CURLcode res;
-  FILE *fp;
 
+  int i = 0;
+  const char *dirs[2];
+  dirs[0] = "blah";
+  dirs[1] = "umm";
+
+  for(i = 0; i < 2; i++){
+    printf("%s\n", dirs[i]);
+  }
+
+  regex_t regex;
+  int reti;
+  char msgbuf[100];
+
+  reti = regcomp(&regex, "^[[:alnum:]]a[[:alnum:]]", 0);
+  if(reti){
+    fprintf(stderr, "Could not compile regex\n");
+    return 1;
+  }
+
+
+  // Execute regular expression
+  reti = regexec(&regex, "helloabc", 0, NULL, 0);
+  if(!reti){
+    printf("%d\n", reti);
+  }
+  else if(reti == REG_NOMATCH){
+    puts("No Match");
+  } else {
+    regerror(reti, &regex, msgbuf, sizeof(msgbuf));
+    fprintf(stderr, "Regex match failed: %s\n", msgbuf);
+    return 1;
+  }
+
+  regfree(&regex);
   curl = curl_easy_init();
-  if(curl){
-    curl_easy_setopt(curl, CURLOPT_URL, url);
+  if(curl) {
+    for(i = 0; i < 2; i++){
+      curl_easy_setopt(curl, CURLOPT_URL, INTRANET_URL);
+      res = curl_easy_perform(curl);
 
-    char *output = basename(args[1]);
-    fp = fopen(output, "wb");
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+      if(CURLE_OK == res) {
+        char *ct;
+        res = curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &ct);
 
-    res = curl_easy_perform(curl);
-    if(res != CURLE_OK){
-      fprintf(stderr, "Could not download file: %s\n", curl_easy_strerror(res));
+        if((CURLE_OK == res) && ct)
+          printf("We received Content-Type: %s\n", ct);
+        else
+          printf("Error");
+      }
     }
 
-    // Cleanup
     curl_easy_cleanup(curl);
   }
   return 0;
 }
+
