2e22a910114b05ea202b2d435cf897b63d506a58
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2018-12-03 23:48:46 +0530

    Changed language C -> C++

--- main.cpp
+++ main.cpp
@@ -9,40 +9,13 @@ int main(void)
   CURL *curl;
   CURLcode res;
 
+  char *url;
+
   int i = 0;
   const char *dirs[2];
   dirs[0] = "blah";
   dirs[1] = "umm";
 
-  for(i = 0; i < 2; i++){
-    printf("%s\n", dirs[i]);
-  }
-
-  regex_t regex;
-  int reti;
-  char msgbuf[100];
-
-  reti = regcomp(&regex, "^[[:alnum:]]a[[:alnum:]]", 0);
-  if(reti){
-    fprintf(stderr, "Could not compile regex\n");
-    return 1;
-  }
-
-
-  // Execute regular expression
-  reti = regexec(&regex, "helloabc", 0, NULL, 0);
-  if(!reti){
-    printf("%d\n", reti);
-  }
-  else if(reti == REG_NOMATCH){
-    puts("No Match");
-  } else {
-    regerror(reti, &regex, msgbuf, sizeof(msgbuf));
-    fprintf(stderr, "Regex match failed: %s\n", msgbuf);
-    return 1;
-  }
-
-  regfree(&regex);
   curl = curl_easy_init();
   if(curl) {
     for(i = 0; i < 2; i++){
@@ -64,4 +37,3 @@ int main(void)
   }
   return 0;
 }
-
